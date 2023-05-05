a15da4e6fd6b1d533d8c8f0c931cf3a5c9b93518
Arjesh <arjesh.khadka2@gmail.com>
2023-05-05 16:34:26 +1000

    home nav  changed

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,18 +4,48 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
 import {GoSignIn} from 'react-icons/go'
+import {GoSignOut} from 'react-icons/go'
 import {FaUserEdit} from 'react-icons/fa'
+import {AiFillDashboard} from 'react-icons/ai'
+import { useDispatch, useSelector } from 'react-redux';
+import { setUser } from '../../pages/signup-signin/userSlice';
+import { signOut } from "firebase/auth";
+import { auth } from "../../config/firebase-config";
 
-const Header = () => (
+const Header = () => {
+
+const dispatch = useDispatch()
+const {user} = useSelector((state)=>state.user)
+
+
+const handleOnSignOut = () => {
+  signOut(auth).then(()=>{
+  dispatch(setUser({}))
+})
+
+}
+
+  return (
   <div>
     <Navbar bg="dark" variant='dark' expand="md">
       <Container>
         <Navbar.Brand href="/"><Link to="/"> E-B</Link></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
+        <Navbar.Collapse id="basic-navbar-nav ms-4">
+          <Nav className="ms-auto ">
+            {!user.uid ?
+            (<>
             <Link to="/signin" className='nav-link'> <GoSignIn className='fs-3' /></Link>
             <Link to="/signup" className='nav-link'> <FaUserEdit className='fs-3'/> </Link>
+            
+            </>
+            ):(
+              <>
+              <Link to="/dashboard" className='nav-link'> <AiFillDashboard className='fs-3' /></Link>
+            <Link to="#" className='nav-link' onClick={handleOnSignOut}> <GoSignOut className='fs-3'/> </Link>
+            </>
+            )
+          }
 
           </Nav>
         </Navbar.Collapse>
@@ -23,5 +53,6 @@ const Header = () => (
     </Navbar>
   </div>
 )
+  }
 
 export default Header
