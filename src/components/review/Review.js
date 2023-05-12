12714154afaf146f73d9401612f4f6c38bcd4edc
Arjesh <arjesh.khadka2@gmail.com>
2023-05-12 23:57:42 +1000

    burrow button and review section

--- src/components/review/Review.js
+++ src/components/review/Review.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import Rating from '../rating/Rating'
+import './review.css'
 
 const Review = () => {
   return (
-    <div className='d-flex justify-content-between gap-2 border p-5 shadow-lg'>
-        <div className='avatar bg-light p-4'>
+    <div className='d-flex justify-content-between gap-2 border p-5 shadow-lg mb-5'>
+        <div className='avatar '>
             <div className='nameLogo'>AK</div>
-            <div className='nameLogo'>Arjesh Khadka</div>
+            <div className='name'>Arjesh Khadka</div>
         </div>
 
-        <div className='review-content'>
+        <div className='review-content '>
             <h4>Best Book</h4>
-            <Rating rate={2}/>
-            <p> LoremhdjkcsdhckozcnhzxLKCnzxKVjxcVhulsduicksdnvcxcm,vnxczviusdfhyvsdaifsidh;fcnlzxkCVxcvuipsdfraefvhahvuvhzxcuibgviyvgfnmsdbzxcviydfovgavgfasho;
+            <Rating rate={2} />
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
             </p>
         </div>
       
--- src/pages/books/BookLanding.js
+++ src/pages/books/BookLanding.js
@@ -1,18 +1,46 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import MainLayout from '../../components/layout/MainLayout'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { Button, Col, Container, Image, Row } from 'react-bootstrap'
 import Rating from '../../components/rating/Rating'
 import Review from '../../components/review/Review'
 
 
+
 const BookLanding = () => {
     const {bookId} = useParams()
-    const {book} = useSelector((state)=> state.books)
+    const navigate = useNavigate()
+    const { book } = useSelector((state)=> state.books)
+    const { user } = useSelector((state)=> state.user)
+
+    useEffect(() =>{
+        if (!book.length) {
+            navigate("/")
+        }
+    })
+
     const selectedBook = book.find(item => item.id === bookId)
     const { id, title, url, name, year, summary} = selectedBook
-    
+
+    const handleOnBurrow =() =>{
+        if(user.uid){
+            //create burrrowhistory table and add following obj
+            const defaultBurrowDay= 14
+            // const obj = {
+            //     bookId,
+            //     bookName,
+            //     userName,
+            //     userId,
+            //     burrowingAt,
+            //     returnAt,
+            //     hasReturned,
+
+            // }
+       
+       
+        }
+    }
     
   return (
    <MainLayout>
@@ -25,11 +53,17 @@ const BookLanding = () => {
 
     <Col sm={7}>
     <h3>{title + " - " + year} </h3>
-    <h4>Author: {name}
-    </h4>
+    <h5>{name}</h5>
     <br/>
     <Rating rate= {5} />
-    <Button className='mt-3 mb-2'>Burrow Now</Button>
+    {!user.uid ? (
+        <Button  className='mt-3 mb-2' disabled={true} >Login to Burrow</Button>
+
+    ):(
+         <Button onClick={handleOnBurrow} className='mt-3 mb-2'>Burrow Now</Button>
+    ) 
+    }
+    
 
     <br/>
     
@@ -45,7 +79,11 @@ const BookLanding = () => {
 
             <h4>Reviews</h4>
             <div className='review-List'>
-                <Review/>
+                <Review />
+                <Review />
+                <Review />
+                <Review />
+                <Review />
             </div>
             </Col>
         </Row>
